046cc9c5328bb74ef8b592545ec53f3a175e7909
alexraju01 <alex.raju1010@gmail.com>
2026-08-05 18:53:48 +0100

    feat: implement user update functionality with Zod validation and enhance user schema

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../db/prisma.js";
+import type { UpdateUserInput } from "@schemas/user.schema.js";
 import type { Request, Response } from "express";
 
 export const getAllUsers = async (_: Request, res: Response) => {
@@ -21,4 +22,21 @@ export const deleteUser = async (req: Request<{ id: string }>, res: Response) =>
   await prisma.user.delete({ where: { id } });
 
   res.status(204).json({ status: "success", data: null });
+};
+export const updateMe = async (req: Request, res: Response) => {
+  const { id: userId } = req.user;
+
+  const { email, image } = req.body as UpdateUserInput;
+  const updatedUser = await prisma.user.update({
+    where: { id: userId },
+    data: {
+      ...(email && { email }),
+      ...(image && { image }),
+    },
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: updatedUser,
+  });
 };
--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -1,4 +1,5 @@
 export * from "./auth.schema.js";
+export * from "./user.schema.js";
 export * from "./availability.schema.js";
 export * from "./teacher.schema.js";
 export * from "./student.schema.js";
